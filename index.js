9442b56b7d79db58d95406eae24a2eb8ab1cfb19
Jose Germán Martínez <jgermanmm@gmail.com>
2023-07-29 14:29:12 -0400

    test02

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*
+You should provide your own project, not the example URL.
+Waiting:You can POST a URL to /api/shorturl and get a JSON response with original_url and short_url properties.
+Waiting:When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+Waiting:If you pass an invalid URL that doesn't follow the valid url format, the JSON response will contain { error: 'invalid url' }
+
+*/
+
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
@@ -18,6 +26,18 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res) {
+
+  res.json({ original_url : 'https://freeCodeCamp.org', short_url : 1}
+  
+  );
+  
+});
+
+
+
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
